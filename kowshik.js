5b80bda39fb15c400b0f43b36060a4bb6569ac45
kowshikeys <go2kowshiktouch@gmail.com>
2022-06-17 21:07:55 +0530

    navbar

---
+++ kowshik.js
@@ -0,0 +1,13 @@
+// dark mode js 
+
+let mode = document.querySelector(".dark-mode");
+ mode.addEventListener("click",() =>{
+     
+     document.body.classList.toggle("dark")
+    
+    document.body.classList.contains("dark") ;{
+         colortheme.style.display ="none";
+         return;
+    }        
+
+ });
